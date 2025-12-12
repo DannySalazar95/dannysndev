17f105019b64ae86674e8fb9f76307861894487a
dannysalazar <danny.namuche.1995@gmail.com>
2025-12-11 21:48:24 -0500

    Ajustes (1)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Intersection Observer para animaciones de scroll
 const observerOptions = {
-    threshold: 0.1,
-    rootMargin: '0px 0px -50px 0px'
+    threshold: 0.05,
+    rootMargin: '0px 0px 50px 0px'
 };
 
 const observer = new IntersectionObserver((entries) => {
